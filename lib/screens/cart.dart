95af6fe981cffbfa257bf723e36e021f521c780c
she-her-intan <intansyameera06@gmail.com>
2024-12-18 13:24:43 +0800

    Update cart.dart-intan

--- lib/screens/cart.dart
+++ lib/screens/cart.dart
@@ -1,1 +1,33 @@
+import 'package:flutter/material.dart';
 
+class CartPage extends StatefulWidget {
+  CartPage({super.key});  // Corrected constructor name
+
+  @override
+  _CartPageState createState() => _CartPageState();
+}
+
+class _CartPageState extends State<CartPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Cart'),
+        centerTitle: true,
+        backgroundColor: Colors.orange,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back), // The back arrow icon
+          onPressed: () {
+            Navigator.pop(context); // Pops the current screen off the stack
+          },
+        ),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: CartPage(),  // Ensure CartPage is called here
+  ));
+}
